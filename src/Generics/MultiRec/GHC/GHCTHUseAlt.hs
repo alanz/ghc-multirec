a063ee8b4bbf0819f3e16dc34a2e570d51893176
Alan Zimmerman <alanz@localhost>
2013-10-13 17:01:53 +0200

    WIP

--- src/Generics/MultiRec/GHC/GHCTHUseAlt.hs
+++ src/Generics/MultiRec/GHC/GHCTHUseAlt.hs
@@ -52,11 +52,13 @@ data AST :: * -> * where
   FixityDirectionIt :: AST (FixityDirection)
   FixityIt          :: AST (HsSyn.Fixity)
   FractionalLitIt   :: AST (FractionalLit)
-  GRHSsIt           :: AST (GRHSs Name)
   GRHSIt            :: AST (GRHS Name)
+  GRHSsIt           :: AST (GRHSs Name)
   HsArrAppTypeIt    :: AST (HsArrAppType)
   HsBindLRIt        :: AST (HsBindLR Name Name)
   HsBracketIt       :: AST (HsBracket Name)
+  HsCmdTopIt        :: AST (HsCmdTop Name)
+  HsConPatDetailsIt :: AST (HsConPatDetails Name)
   HsGroupIt         :: AST (HsGroup Name)
   HsIPBindsIt       :: AST (HsIPBinds Name)
   HsIPNameIt        :: AST (HsIPName)
@@ -68,9 +70,18 @@ data AST :: * -> * where
   HsRecordBindsIt   :: AST (HsRecordBinds Name)
   HsSpliceIt        :: AST (HsSplice Name)
   HsStmtContextIt   :: AST (HsStmtContext Name)
+  HsTupArgIt        :: AST (HsTupArg Name)
+  HsTypeIt          :: AST (HsType Name)
   HsValBindsLRIt    :: AST (HsValBindsLR Name Name)
   IEIt              :: AST (IE Name)
   ImportDeclIt      :: AST (ImportDecl Name)
+  IPBindIt          :: AST (IPBind Name)
+  LAnnDeclIt        :: AST (LAnnDecl Name)
+  LDefaultDeclIt    :: AST (LDefaultDecl Name)
+  LDerivDeclIt      :: AST (LDerivDecl Name)
+  LDocDecl          :: AST LDocDecl
+  LFixitySigIt      :: AST (LFixitySig Name)
+  LForeignDeclIt    :: AST (LForeignDecl Name)
   LGRHSIt           :: AST (LGRHS Name)
   LHsBindLRIt       :: AST (LHsBindLR Name Name)
   LHsBindsIt        :: AST (LHsBinds Name)
@@ -81,83 +92,79 @@ data AST :: * -> * where
   LIEIt             :: AST (LIE Name)
   LIPBindIt         :: AST (LIPBind Name)
   LImportDeclIt     :: AST (LImportDecl Name)
-  LInstDeclIt       :: AST ( LInstDecl Name)
+  LInstDeclIt       :: AST (LInstDecl Name)
   LMatchIt          :: AST (LMatch Name)
   LPatIt            :: AST (LPat Name)
+  LRuleDeclIt       :: AST (LRuleDecl Name)
   LSigIt            :: AST (LSig Name)
   LStmtLRIt         :: AST (LStmtLR Name Name)
-
-  LFixitySigIt      :: AST (LFixitySig Name)
-  LAnnDeclIt        :: AST (LAnnDecl Name)
-  LDefaultDeclIt    :: AST (LDefaultDecl Name)
-  LDerivDeclIt      :: AST (LDerivDecl Name)
-  LForeignDeclIt    :: AST (LForeignDecl Name)
-  LRuleDeclIt       :: AST (LRuleDecl Name)
   LTyClDeclIt       :: AST (LTyClDecl Name)
   LVectDeclIt       :: AST (LVectDecl Name)
   LWarnDeclIt       :: AST (LWarnDecl Name)
-  LDocDecl          :: AST LDocDecl
-
   LocatedIt         :: AST (Located Name)
-
-
   MatchGroupIt      :: AST (MatchGroup Name)
   MatchIt           :: AST (Match Name)
   NameIt            :: AST (Name)
   OverLitValIt      :: AST (OverLitVal)
+  ParStmtBlockIt    :: AST (ParStmtBlock Name Name)
   PatIt             :: AST (Pat Name)
   PostTcExprIt      :: AST PostTcExpr
-  -- PostTcTypeIt      :: AST PostTcType
   RenamedSourceIt   :: AST RenamedSource
   StmtLRIt          :: AST (StmtLR Name Name)
   SyntaxExprIt      :: AST (SyntaxExpr Name)
   TcEvBindsIt       :: AST (TcEvBinds)
   TickishIt         :: AST (Tickish Name)
+  TyClDeclIt        :: AST (TyClDecl Name)
 
   -- Maybe elements
   MaybeBoolLIENamesIt :: AST (Maybe ((Bool, [LIE Name])))
+  MaybeLHsExprIt      :: AST (Maybe (LHsExpr Name))
+  MaybeLHsTypeIt      :: AST (Maybe (LHsType Name)) -- Includes LHsKind
+  MaybeSyntaxExprIt   :: AST (Maybe (SyntaxExpr Name))
 
   -- List elements
-  ABExportListIt    :: AST [ABExport Name]
-  LGRHSListIt       :: AST [LGRHS Name]
-  LHsBindLRListIt   :: AST [LHsBindLR Name Name]
-  LHsCmdTopListIt   :: AST [LHsCmdTop Name]
-  LHsDeclListIt     :: AST [LHsDecl Name]
-  LHsExprListIt     :: AST [LHsExpr Name]
-  LIEListIt         :: AST [LIE Name]
-  LIPBindListIt     :: AST [LIPBind Name]
-  LImportDeclListIt :: AST [LImportDecl Name]
-  LInstDeclListIt   :: AST [LInstDecl Name]
-  LMatchListIt      :: AST [LMatch Name]
-  LPatListIt        :: AST [LPat Name]
-  LSigListIt        :: AST [LSig Name]
-  LStmtListIt       :: AST [LStmt Name]
-  LTyClDeclListIt   :: AST [LTyClDecl Name]
-  -- RecBindsListIt    :: AST [HsRecField Name (GenLocated SrcSpan (HsExpr Name))]
-  RecBindsListIt    :: AST [HsRecField Name (LHsExpr Name)]
-  ValBindsOutListIt :: AST [(RecFlag, LHsBinds Name)]
-
-  LTyClDeclLitListIt :: AST [[LTyClDecl Name]]
-  LDerivDeclListIt   :: AST [LDerivDecl Name]
-  LFixitySigLitsIt   :: AST [LFixitySig Name]
-  LDefaultDeclListIt :: AST [LDefaultDecl Name]
-  LForeignDeclListIt :: AST [LForeignDecl Name]
-  LWarnDeclListIt    :: AST [LWarnDecl Name]
+  ABExportListIt     :: AST [ABExport Name]
   LAnnDeclListIt     :: AST [LAnnDecl Name]
-  LRuleDeclListIt    :: AST [LRuleDecl Name]
-  LVectDeclListIt    :: AST [LVectDecl Name]
+  LDefaultDeclListIt :: AST [LDefaultDecl Name]
+  LDerivDeclListIt   :: AST [LDerivDecl Name]
   LDocDeclListIt     :: AST [LDocDecl]
-
+  LFixitySigLitsIt   :: AST [LFixitySig Name]
+  LForeignDeclListIt :: AST [LForeignDecl Name]
+  LGRHSListIt        :: AST [LGRHS Name]
+  LHsBindLRListIt    :: AST [LHsBindLR Name Name]
+  LHsCmdTopListIt    :: AST [LHsCmdTop Name]
+  LHsDeclListIt      :: AST [LHsDecl Name]
+  LHsExprListIt      :: AST [LHsExpr Name]
+  LIEListIt          :: AST [LIE Name]
+  LIPBindListIt      :: AST [LIPBind Name]
+  LImportDeclListIt  :: AST [LImportDecl Name]
+  LInstDeclListIt    :: AST [LInstDecl Name]
+  LMatchListIt       :: AST [LMatch Name]
+  LPatListIt         :: AST [LPat Name]
+  LRuleDeclListIt    :: AST [LRuleDecl Name]
+  LSigListIt         :: AST [LSig Name]
+  LStmtListIt        :: AST [LStmt Name]
+  LTyClDeclListIt    :: AST [LTyClDecl Name]
+  LTyClDeclLitListIt :: AST [[LTyClDecl Name]]
+  LVectDeclListIt    :: AST [LVectDecl Name]
+  LWarnDeclListIt    :: AST [LWarnDecl Name]
+  RecBindsListIt     :: AST [HsRecField Name (LHsExpr Name)]
+  ValBindsOutListIt  :: AST [(RecFlag, LHsBinds Name)]
+  VarListIt           :: AST [Var] -- Includes TyVar and EvVar
+  HsTupArgListIt      :: AST [HsTupArg Name]
+  DataConListIt       :: AST [DataCon]
+  PendingSpliceListIt :: AST [PendingSplice]
+  ParStmtBlockListIt  :: AST [ParStmtBlock Name Name]
 
   -- Tuple elements
   TupRecFlagLHsBindsIt :: AST (RecFlag, LHsBinds Name)
+  TupBoolLIEListIt     :: AST (Bool, [LIE Name])
 
 $(deriveEverything
   (DerivOptions {
    familyTypes =
         [ ( [t| ABExport Name          |], "ABExportIt" )
         , ( [t| ArithSeqInfo Name      |], "ArithSeqInfoIt"   )
-
         , ( [t| Boxity                 |], "BoxityIt" )
         , ( [t| FixityDirection        |], "FixityDirectionIt" )
         , ( [t| FractionalLit          |], "FractionalLitIt" )
@@ -166,6 +173,8 @@ $(deriveEverything
         , ( [t| HsArrAppType           |], "HsArrAppTypeIt"   )
         , ( [t| HsBindLR Name Name     |], "HsBindLRIt" )
         , ( [t| HsBracket Name         |], "HsBracketIt" )
+        , ( [t| HsCmdTop Name          |], "HsCmdTopIt" )
+        , ( [t| HsConPatDetails Name   |], "HsConPatDetailsIt" )
         , ( [t| HsGroup Name           |], "HsGroupIt" )
         , ( [t| HsIPBinds Name         |], "HsIPBindsIt" )
         , ( [t| HsIPName               |], "HsIPNameIt" )
@@ -178,9 +187,12 @@ $(deriveEverything
         , ( [t| HsSplice Name          |], "HsSpliceIt"   )
         , ( [t| HsStmtContext Name     |], "HsStmtContextIt" )
         , ( [t| HsSyn.Fixity           |], "FixityIt" )
+        , ( [t| HsTupArg Name          |], "HsTupArgIt" )
+        , ( [t| HsType Name            |], "HsTypeIt" )
         , ( [t| HsValBindsLR Name Name |], "HsValBindsLRIt" )
         , ( [t| IE Name                |], "IEIt" )
         , ( [t| ImportDecl Name        |], "ImportDeclIt"   )
+        , ( [t| IPBind Name            |], "IPBindIt" )
         , ( [t| LAnnDecl Name          |], "LAnnDeclIt" )
         , ( [t| LDefaultDecl Name      |], "LDefaultDeclIt" )
         , ( [t| LDerivDecl Name        |], "LDerivDeclIt" )
@@ -210,6 +222,7 @@ $(deriveEverything
         , ( [t| Match Name             |], "MatchIt" )
         , ( [t| MatchGroup Name        |], "MatchGroupIt" )
         , ( [t| OverLitVal             |], "OverLitValIt"   )
+        , ( [t| ParStmtBlock Name Name |], "ParStmtBlockIt" )
         , ( [t| Pat Name               |], "PatIt" )
         , ( [t| PostTcExpr             |], "PostTcExprIt" )
         , ( [t| RenamedSource          |], "RenamedSourceIt" )
@@ -217,43 +230,52 @@ $(deriveEverything
         , ( [t| SyntaxExpr Name        |], "SyntaxExprIt" )
         , ( [t| TcEvBinds              |], "TcEvBindsIt" )
         , ( [t| Tickish Name           |], "TickishIt"   )
-
+        , ( [t| (TyClDecl Name)        |], "TyClDeclIt" )
 
         -- List elements
         , ( [t| [(RecFlag, LHsBinds Name)] |], "ValBindsOutListIt"   )
-        , ( [t| [ABExport Name]       |],   "ABExportListIt" )
-        , ( [t| [LGRHS Name]          |],   "LGRHSListIt" )
-        , ( [t| [LHsDecl Name]        |],   "LHsDeclListIt" )
-        , ( [t| [LHsExpr Name]        |],   "LHsExprListIt" )
-        , ( [t| [LIPBind Name]        |],   "LIPBindListIt" )
-        , ( [t| [LInstDecl Name]      |],   "LInstDeclListIt" )
-        , ( [t| [LSig Name]           |],   "LSigListIt" )
-        , ( [t| [LStmt Name]          |],   "LStmtListIt" )
-        , ( [t| [LTyClDecl Name]      |],   "LTyClDeclListIt" )
-        , ( [t| [LPat Name]           |],   "LPatListIt"        )
-        -- , ( [t| [HsRecField Name (GenLocated SrcSpan (HsExpr Name))] |],   "RecBindsListIt"    )
+        , ( [t| [ABExport Name]            |],   "ABExportListIt" )
+        , ( [t| [DataCon]                  |],   "DataConListIt" )
         , ( [t| [HsRecField Name (LHsExpr Name)] |],   "RecBindsListIt"    )
-        , ( [t| [LMatch Name]         |],   "LMatchListIt"      )
-        , ( [t| [LHsCmdTop Name]      |],   "LHsCmdTopListIt"   )
-        , ( [t| [LHsBindLR Name Name] |],   "LHsBindLRListIt"   )
-        , ( [t| [LImportDecl Name]    |],   "LImportDeclListIt" )
-        , ( [t| [LIE Name]            |],   "LIEListIt" )
-        , ( [t| [[LTyClDecl Name]]    |],   "LTyClDeclLitListIt" )
-        , ( [t| [LDerivDecl Name]    |],   "LDerivDeclListIt" )
-        , ( [t| [LFixitySig Name]    |],   "LFixitySigLitsIt" )
-        , ( [t| [LDefaultDecl Name]    |],   "LDefaultDeclListIt" )
-        , ( [t| [LForeignDecl Name]    |],   "LForeignDeclListIt" )
-        , ( [t| [LWarnDecl Name]    |],   "LWarnDeclListIt" )
-        , ( [t| [LAnnDecl Name]    |],   "LAnnDeclListIt" )
-        , ( [t| [LRuleDecl Name]    |],   "LRuleDeclListIt" )
-        , ( [t| [LVectDecl Name]    |],   "LVectDeclListIt" )
-        , ( [t| [LDocDecl]    |],   "LDocDeclListIt" )
-
+        , ( [t| [HsTupArg Name]            |],   "HsTupArgListIt" )
+        , ( [t| [LAnnDecl Name]            |],   "LAnnDeclListIt" )
+        , ( [t| [LDefaultDecl Name]        |],   "LDefaultDeclListIt" )
+        , ( [t| [LDerivDecl Name]          |],   "LDerivDeclListIt" )
+        , ( [t| [LDocDecl]                 |],   "LDocDeclListIt" )
+        , ( [t| [LFixitySig Name]          |],   "LFixitySigLitsIt" )
+        , ( [t| [LForeignDecl Name]        |],   "LForeignDeclListIt" )
+        , ( [t| [LGRHS Name]               |],   "LGRHSListIt" )
+        , ( [t| [LHsBindLR Name Name]      |],   "LHsBindLRListIt"   )
+        , ( [t| [LHsCmdTop Name]           |],   "LHsCmdTopListIt"   )
+        , ( [t| [LHsDecl Name]             |],   "LHsDeclListIt" )
+        , ( [t| [LHsExpr Name]             |],   "LHsExprListIt" )
+        , ( [t| [LIE Name]                 |],   "LIEListIt" )
+        , ( [t| [LIPBind Name]             |],   "LIPBindListIt" )
+        , ( [t| [LImportDecl Name]         |],   "LImportDeclListIt" )
+        , ( [t| [LInstDecl Name]           |],   "LInstDeclListIt" )
+        , ( [t| [LMatch Name]              |],   "LMatchListIt"      )
+        , ( [t| [LPat Name]                |],   "LPatListIt"        )
+        , ( [t| [LRuleDecl Name]           |],   "LRuleDeclListIt" )
+        , ( [t| [LSig Name]                |],   "LSigListIt" )
+        , ( [t| [LStmt Name]               |],   "LStmtListIt" )
+        , ( [t| [LTyClDecl Name]           |],   "LTyClDeclListIt" )
+        , ( [t| [LVectDecl Name]           |],   "LVectDeclListIt" )
+        , ( [t| [LWarnDecl Name]           |],   "LWarnDeclListIt" )
+        , ( [t| [ParStmtBlock Name Name]   |],   "ParStmtBlockListIt" )
+        , ( [t| [PendingSplice]            |],   "PendingSpliceListIt" )
+        , ( [t| [Var]                      |],   "VarListIt" )
+        , ( [t| [[LTyClDecl Name]]         |],   "LTyClDeclLitListIt" )
 
         -- Maybe elements
         , ( [t| Maybe ((Bool, [LIE Name])) |],   "MaybeBoolLIENamesIt" )
+        , ( [t| Maybe (LHsExpr Name)       |],   "MaybeLHsExprIt" )
+        -- , ( [t| Maybe (LHsKind Name)       |],   "MaybeLHsKindIt" )
+        , ( [t| Maybe (LHsType Name)       |],   "MaybeLHsTypeIt" )
+        , ( [t| Maybe (SyntaxExpr Name)    |],   "MaybeSyntaxExprIt" )
+
         -- Tuple elements
         , ( [t| (RecFlag, LHsBinds Name) |],   "TupRecFlagLHsBindsIt" )
+        , ( [t| (Bool, [LIE Name])       |],   "TupBoolLIEListIt" )
 
         ],
    indexGadtName = "AST",
